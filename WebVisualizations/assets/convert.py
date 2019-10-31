9afa0520118e9b9e9265c8d1173ba28035620904
Andrew Sooklall <andrewsooklall@gmail.com>
2019-10-31 18:43:10 -0400

    updated for pages

---
+++ WebVisualizations/assets/convert.py
@@ -0,0 +1,11 @@
+# Converts CSV to DataFrame to HTML
+
+import numpy as np
+import pandas as pd
+
+#load csv and prepare dataframe.
+file = "cities.csv"
+cities_df = pd.read_csv(file, delimiter = ',')
+
+#conversion and directory
+cities_df.to_html('../resources/cities_data.html', index=False)
